91c11629305623f1dc04adf1043bdcc4d3c92c07
SR9401 <shakibrojas@icloud.com>
2025-07-28 13:06:33 +0200

    push task 1

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
 
 def concat(str1: str, str2: str) -> str:
-	"""function to concat string"""
-	return str1 + str2
+    """function to concat string"""
+    return str1 + str2
