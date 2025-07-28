939c694485dd779e0a236791f88cfbcf5e59fca1
SR9401 <shakibrojas@icloud.com>
2025-07-28 18:32:41 +0200

    coreect pycodestyle

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """0. Basic annotations - add"""
 
+
 def add(a: float, b: float) -> float:
     """function to add float"""
     return a + b
--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """3. Basic annotations - to string"""
 
+
 def to_str(n: float) -> str:
     """function to convert a float to str"""
     return str(n)
