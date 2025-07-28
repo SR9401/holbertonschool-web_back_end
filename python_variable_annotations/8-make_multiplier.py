8c5be913fc510d390365d4d379bf33316ee65b31
SR9401 <shakibrojas@icloud.com>
2025-07-28 18:29:32 +0200

    correct task 8

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -5,7 +5,8 @@ from typing import Callable
 
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
-    
+    """function that multipliate"""
     def multiplier_function(x: float) -> float:
+        """function that multipliate"""
         return x * multiplier
     return multiplier_function
