3252f56447bfa1d9871f651c83cc794dbfc433aa
SR9401 <shakibrojas@icloud.com>
2025-07-28 17:55:05 +0200

    push task 7

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
-from typing import Tuple
+from typing import Tuple, Union
 
 
-def to_kv(k: str, v: int | float) -> Tuple[str, int | float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """Function to return a tuple with the string k and the square of v as a float."""
     return (k, (v * v)) 
