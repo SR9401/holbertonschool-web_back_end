23c3cbcae8002fb369c4ffba23dacec3fd83a17a
SR9401 <shakibrojas@icloud.com>
2025-07-28 15:31:04 +0200

    push task 6

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+from typing import Union, List
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """function that add element of mixed type list"""
+    return sum(mxd_lst)
