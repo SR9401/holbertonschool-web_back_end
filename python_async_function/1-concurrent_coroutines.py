6067cca5066d5a450fcb6fa6d2f3b235004aed76
SR9401 <shakibrojas@icloud.com>
2025-07-29 16:53:41 +0200

    correct task 1 4

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -3,24 +3,16 @@
 
 import asyncio
 from typing import List
-
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """ write an async routine called wait_n that takes in 2 int arguments"""
+    """Spawn wait_random n times with the specified max_delay"""
+    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
+    delays = []
 
-    task = []
+    for task in asyncio.as_completed(tasks):
+        result = await task
+        delays.append(result)
 
-    for _ in range(n):
-        task = asyncio.create_task(wait_random(max_delay))
-        task = task.append(task)
-
-    delais = await asyncio.gather(*task)
-
-    a = len(delais)
-    for i in range(a):
-        for j in range(0, a-i-1):
-            if delais[j] > delais[j+1]:
-                delais[j], delais[j+1] = delais[j+1], delais[j]
-    return delais
+    return delays
