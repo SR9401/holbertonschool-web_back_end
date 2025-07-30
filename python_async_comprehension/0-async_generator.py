a8f4635504405355abbf832ca5cc3f882dd212ed
SR9401 <shakibrojas@icloud.com>
2025-07-30 11:51:01 +0200

    push task 0

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""0. Async Generator"""
+
+import asyncio
+import random
+
+
+async def async_generator():
+	"""Write a coroutine called async_generator that takes no arguments."""
+	for _ in range(10):
+		await asyncio.sleep(1)
+		yield(random.uniform(0,10))
+
