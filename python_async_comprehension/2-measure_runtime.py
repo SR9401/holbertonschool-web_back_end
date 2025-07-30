685b1d8549cb764ad2730623ba355d1a32332cd6
SR9401 <shakibrojas@icloud.com>
2025-07-30 13:51:43 +0200

    push task 2

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -7,10 +7,10 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime():
-	"""write a measure_runtime coroutine that will execute async_comprehension"""
-	start_time = time.time()
-	await asyncio.gather(*(async_comprehension() for _ in range(4)))
-	end_time = time.time()
+    """write a measure_runtime coroutine that will execute async_comprehension"""
+    start_time = time.time()
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
+    end_time = time.time()
 
-	exec_time = end_time - start_time
-	return exec_time
+    exec_time = end_time - start_time
+    return exec_time
