70fc4d5be659a71b3d65c774f68e0251471e9e2e
SR9401 <shakibrojas@icloud.com>
2025-07-30 13:01:39 +0200

    push task 1

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-""""""
+"""1. Async Comprehensions"""
 import asyncio
 from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
-    """"""
+    """rite a coroutine called async_comprehension that takes no arguments."""
     my_list = []
     async for i in async_generator():
         my_list.append(i)
