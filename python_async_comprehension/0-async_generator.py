f24f29bd2f9fb87db0400e017a021552f47c95e4
SR9401 <shakibrojas@icloud.com>
2025-07-30 14:04:22 +0200

    test task 0

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -3,9 +3,10 @@
 
 import asyncio
 import random
+from typing import AsyncGenerator
 
 
-async def async_generator():
+async def async_generator() -> AsyncGenerator[int, None]:
     """Write a coroutine called async_generator that takes no arguments."""
     for _ in range(10):
         await asyncio.sleep(1)
