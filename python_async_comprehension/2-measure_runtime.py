c77c2deea6aceccd98664ef02bba2625a794fe55
SR9401 <shakibrojas@icloud.com>
2025-07-30 13:53:06 +0200

    correct task 2

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -7,7 +7,7 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime():
-    """write a measure_runtime coroutine that will execute async_comprehension"""
+    """measure_runtime coroutine that will execute async_comprehension"""
     start_time = time.time()
     await asyncio.gather(*(async_comprehension() for _ in range(4)))
     end_time = time.time()
