715b1f778d37aa580232754799f957defa394688
SR9401 <shakibrojas@icloud.com>
2025-07-31 09:49:18 +0200

    correct task1

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -7,5 +7,7 @@ async_generator = __import__('0-async_generator').async_generator
 
 async def async_comprehension() -> List[float]:
     """rite a coroutine called async_comprehension that takes no arguments."""
-
-    return [n async for n in async_generator]
+    my_list = []
+    async for i in async_generator():
+        my_list.append(i)
+    return my_list
