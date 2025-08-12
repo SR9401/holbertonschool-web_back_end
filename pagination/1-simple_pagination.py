921c32762a862e700a4fd7b37f0b911b823f7a39
SR9401 <shakibrojas@icloud.com>
2025-08-12 12:06:34 +0200

    push task 1

---
+++ pagination/1-simple_pagination.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+'''0. Simple helper function'''
+
+import csv
+import math
+from typing import List
+
+
+def index_range(page: int, page_size: int) -> tuple:
+    '''Write a function index_range takes twoarguments page and page_size.
+    '''
+    res = ((page * page_size) - page_size)
+    resp = res + page_size
+    r = (res, resp)
+    return r
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """fun to verify arguments and to assign start and end"""
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+        data = self.dataset()
+        start, end = index_range(page, page_size)
+        return data[start:end]
