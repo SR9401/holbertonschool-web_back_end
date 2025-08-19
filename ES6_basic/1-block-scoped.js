a1f3473fcb52ee3d67587538b05e79c59ee98eed
SR9401 <shakibrojas@icloud.com>
2025-08-19 11:08:47 +0200

    push task 1

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -3,8 +3,8 @@ export default function taskBlock(trueOrFalse) {
   let task2 = true;
 
   if (trueOrFalse) {
-    let task4 = true;
-    let task3 = false;
+    let task = false;
+    let task2 = true;
   }
 
   return [task, task2];
