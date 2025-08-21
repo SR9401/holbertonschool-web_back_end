56ba408d6577ed87f63776cde0cec8f8a6ef1e12
SR9401 <shakibrojas@icloud.com>
2025-08-21 10:37:30 +0200

    push task 3

---
+++ ES6_classes/3-currency.js
@@ -0,0 +1,38 @@
+export default class Currency {
+  constructor(code, name) {
+    if (typeof code !== 'string') {
+      throw TypeError('Code must be a string');
+    }
+    if (typeof name !== 'string') {
+      throw TypeError('Name must be a string');
+    }
+    this._code = code;
+    this._name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set code(ncode) {
+    if (typeof ncode !== 'string') {
+      throw TypeError('Code muste be a string');
+    }
+    this._code = ncode;
+  }
+
+  set name(nName) {
+    if (typeof nName !== 'string') {
+      throw TypeError('Name must be a string');
+    }
+    this._name = nName;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
