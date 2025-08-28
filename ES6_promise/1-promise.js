e954df7e52bdaf9ff52eca29a4c2ae9453dd06af
SR9401 <shakibrojas@icloud.com>
2025-08-28 11:48:27 +0200

    push task 1

---
+++ ES6_promise/1-promise.js
@@ -0,0 +1,14 @@
+export default function getFullResponseFromAPI(success) {
+	return new Promise((resolve, reject) => {
+		if (success === true ) {
+			const resp = {
+				status: 200,
+				body: 'Succes'
+			};
+			resolve(resp);
+		}
+		else {
+			reject(new Error('The fake API is not working currently'))
+		}
+	});
+}
