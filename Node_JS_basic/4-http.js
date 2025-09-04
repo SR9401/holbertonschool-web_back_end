dfdb97b59cf80df177caad8f18bac41ddf3aaabf
SR9401 <shakibrojas@icloud.com>
2025-09-04 09:26:48 +0200

    push task 4

---
+++ Node_JS_basic/4-http.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!');
+});
+const app = server;
+
+app.listen(1245, '127.0.0.1', () => {
+  console.log('Server running at http://127.0.0.1:1245/');
+});
